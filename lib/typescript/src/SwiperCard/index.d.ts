1f546d2558df8a6698bbc87bc5e0ebea1732b189
Srikanth Gopalakrishnan <srikanth.gokr@gmail.com>
2026-07-09 16:31:00 +0530

    Delete lib folder from upstream

--- lib/typescript/src/SwiperCard/index.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import type { SwiperCardOptions } from '../types.js';
-import type { SwiperCardInternalRefType } from '../internalTypes.js';
-declare const SwipeableCard: React.ForwardRefExoticComponent<SwiperCardOptions<unknown> & {
-    children?: React.ReactNode | undefined;
-} & React.RefAttributes<SwiperCardInternalRefType>>;
-declare const _default: typeof SwipeableCard;
-export default _default;
-//# sourceMappingURL=index.d.ts.map
--- lib/typescript/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { default as Swiper } from './Swiper.js';
-export type { SwiperSwipeDirection, SwiperRestoredSwipe, SwiperCardRefType, SwiperOptions, SwiperCardOptions, } from './types.js';
-//# sourceMappingURL=index.d.ts.map
